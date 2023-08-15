a94cb14c990d34bc69bba89f2f0abaa256656f39
x3osama <140915735+x3osama@users.noreply.github.com>
2023-08-15 14:42:54 +0300

    Add files via upload

---
+++ jjjsondata.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Fetch JSON data
+    fetch("chicken-data.json")
+        .then(response => response.json())
+        .then(data => displayData(data))
+        .catch(error => console.error("Error fetching JSON data:", error));
+
+
+});
+
+function displayData(data) {
+    const jsonDataDiv = document.getElementById("json-data");
+    jsonDataDiv.innerHTML = ""; // Clear any existing content
+
+    data.forEach(item => {
+        const itemDiv = document.createElement("div");
+        itemDiv.classList.add("json-item");
+
+        const nameDiv = document.createElement("h2");
+        nameDiv.classList.add("name");
+        nameDiv.textContent = item.name;
+
+        const descriptionDiv = document.createElement("div");
+        descriptionDiv.classList.add("description");
+        descriptionDiv.textContent = item.description;
+
+        itemDiv.appendChild(nameDiv);
+        itemDiv.appendChild(descriptionDiv);
+
+        jsonDataDiv.appendChild(itemDiv);
+    });
+// if (performance.navigation.type === 1) {
+//   setTimeout(function() {
+//     window.location.href = "training.html";
+//   }, 100);
+// }
+
+}
---
+++ test.js
@@ -0,0 +1,19 @@
+clickableItem_beef = document.getElementById('beef');
+clickableItem_chicken = document.getElementById('chicken');
+clickableItem_sushi = document.getElementById('sushi');
+
+clickableItem_beef.addEventListener('click', function() {
+ window.location.href="jsondata1.html"});
+
+clickableItem_chicken.addEventListener('click', function() {
+ window.location.href="jsondata.html"});
+
+ clickableItem_sushi.addEventListener('click', function() {
+     window.location.href = "jsondata2.html"});
+
+
+
+
+
+
+
